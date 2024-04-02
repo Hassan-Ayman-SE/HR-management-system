61ad2e566b11dde5cf92f2d6fa3a2634afb22568
Hassan-Ayman-SE <hsankraz66@gmail.com>
2024-04-02 01:46:44 +0000

    FIX issue AND UPDATE js file

--- app.js
+++ app.js
@@ -1,62 +1,101 @@
+let main = document.querySelector('main');
+let employees = [];
 
 function generateAUniqueId() {
-    return 1000 + employees.length;
+  return 1000 + employees.length;
 }
 
 function generateRandomSalary(level) {
-    let minSalary, maxSalary;
+  let minSalary, maxSalary;
 
-    switch (level) {
-        case "Senior":
-            minSalary = 1500;
-            maxSalary = 2000;
-            break;
-        case "Mid-Senior":
-            minSalary = 1000;
-            maxSalary = 1500;
-            break;
-        case "Junior":
-            minSalary = 500;
-            maxSalary = 1000;
-            break;
-    }
+  switch (level) {
+    case "Senior":
+      minSalary = 1500;
+      maxSalary = 2000;
+      break;
+    case "Mid-Senior":
+      minSalary = 1000;
+      maxSalary = 1500;
+      break;
+    case "Junior":
+      minSalary = 500;
+      maxSalary = 1000;
+      break;
+  }
 
-    return Math.floor(Math.random() * (maxSalary - minSalary + 1) + minSalary);
+  return Math.floor(Math.random() * (maxSalary - minSalary + 1) + minSalary);
 }
-let employees = [];
-const depts = ['Administration', 'Finance', 'Development', 'Marketing'];
-// Employee 
-class Employee {
-    //constructor
+
+class Employee{
     constructor(fullName, department, level, imageUrl) {
         this.id = generateAUniqueId();
         this.fullName = fullName;
         this.department = department;
         this.level = level;
         this.imageUrl = imageUrl;
-        this.salary = generateRandomSalary();
+        this.salary = generateRandomSalary(level);
         employees.push(this);
     }
-
-
-
 }
 
-Employee.prototype.calculateSalary = function () {
-    const sal = generateRandomSalary(this.level);
-    const taxPercent = 7.5;
-    const taxAmount = (sal * taxPercent) / 100;
-    const netSalary = sal - taxAmount;
+Employee.prototype.calculateSalary = function() {
+  const taxPercent = 7.5;
+  const taxAmount = (this.salary * taxPercent) / 100;
+  const netSalary = this.salary - taxAmount;
 
-    return netSalary;
+  return netSalary;
+};
+
+Employee.prototype.render = function() {
+  let cardDiv = document.createElement("div");
+  cardDiv.classList.add("my-card");
+  cardDiv.innerHTML = `
+    <img src="${this.imageUrl}">
+    <div class="card-content">
+      <p>Name: ${this.fullName} - ID: ${this.id}</p>
+      <p>Department: ${this.department} - Level: ${this.level}</p>
+      <p>${this.calculateSalary()}</p>
+    </div>
+  `;
+  
+  let departmentSection = document.getElementById(this.department);
+  console.log(departmentSection);
+  if (departmentSection) {
+    departmentSection.appendChild(cardDiv);
+  } else {
+    let deptSection = document.createElement("section");
+    deptSection.id = this.department;
+    deptSection.classList.add("employees");
+    let deptHeading = document.createElement("h2");
+    deptHeading.textContent = this.department;
+    deptSection.appendChild(deptHeading);
+    deptSection.appendChild(cardDiv);
+    main.appendChild(deptSection);
+  }
+};
+
+function renderEmployees() {
+  employees.forEach((emp) => {
+    emp.render();
+  });
 }
 
-Employee.prototype.renderNameAndSalary = function () {
-    return `${this.fullName} ${this.salary}`;
+const empForm = document.getElementById("empForm");
+empForm.addEventListener('submit', addNewEmpHandler);
+
+function addNewEmpHandler(event) {
+  event.preventDefault();
+
+  const fullName = event.target.fullname.value;
+  const department = event.target.department.value;
+  const level = event.target.level.value;
+  const imageUrl = event.target.imageurl.value;
+
+  const newEmp = new Employee(fullName, department, level, imageUrl);
+  newEmp.render();
 }
 
-
-
+// Employees Object
 new Employee("Ghazi Samer", "Administration", "Senior", "https://cdn-icons-png.flaticon.com/128/4140/4140048.png");
 new Employee("Lana Ali", "Finance", "Senior", "https://cdn-icons-png.flaticon.com/128/4140/4140047.png");
 new Employee("Tamara Ayoub", "Marketing", "Senior", "https://cdn-icons-png.flaticon.com/128/4140/4140051.png");
@@ -65,68 +104,4 @@ new Employee("Omar Zaid", "Development", "Senior", "https://cdn-icons-png.flatic
 new Employee("Rana Saleh", "Development", "Junior", "https://cdn-icons-png.flaticon.com/128/4140/4140047.png");
 new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "https://cdn-icons-png.flaticon.com/128/4202/4202831.png");
 
-// let tbl = document.getElementsByTagName("tbody");
-// for(let i = 0; i < employees.length; i++){
-//     let emp = employees[i];
-//     tbl[0].innerHTML += `
-//     <tr>
-//     <td>${emp.id}</td>
-//     <td>${emp.fullName}</td>
-//     <td>${emp.department}</td>
-//     <td>${emp.level}</td>
-//     <td><img src="${emp.imageUrl}" alt="img"></td>
-//     <td>${emp.calculateSalary()}</td>
-//     <td>${emp.salary}</td>
-//     </tr>
-//     `;
-// }
-// console.log(tbl[0].innerHTML);
-
-let f = document.forms[0];
-f.addEventListener("submit", function (onSubmit) {
-    let fullName = onSubmit.target.fullName.value;
-    let department = onSubmit.target.department.value;
-    let level = onSubmit.target.level.value;
-    let imageUrl = onSubmit.target.imageUrl.value;
-    if (fullName === "" || department === "") {
-        onSubmit.preventDefault();
-    } else {
-        new Employee(fullName, department, level, imageUrl);
-    }
-});
-let main = document.querySelector('main');
-
-depts.forEach(department => {
-    // Filtering by depts
-    let departmentEmps = employees.filter(employee => employee.department === department);
-
-    // deptSection
-    let deptSection = document.createElement("section");
-    deptSection.classList.add("employees");
-
-    //department heading
-    let deptHeading = document.createElement("h2");
-    deptHeading.textContent = department;
-    deptSection.appendChild(deptHeading);
-
-    // Append employee cards to the section
-    departmentEmps.forEach(emp => {
-        let firstName = emp.fullName.split(" ")[0];//becuase images in asset is the first name of employee
-        let cardDiv = document.createElement("div");
-        cardDiv.classList.add("my-card");
-        cardDiv.innerHTML = `
-            <img src="assets/${firstName}.jpg">
-            <div class="card-content">
-                <p>Name: ${emp.fullName} - ID: ${emp.id}</p>
-                <p>Department: ${emp.department} - Level: ${emp.level}</p>
-                <p>1640</p>
-            </div>
-            `
-            ;
-        deptSection.appendChild(cardDiv);
-    });
-
-    // Append to Main
-    main.appendChild(deptSection);
-});
-
+renderEmployees();
